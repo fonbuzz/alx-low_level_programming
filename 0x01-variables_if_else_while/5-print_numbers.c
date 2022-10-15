f04dc8a77781d2610567a28576511bc27feb8cea
fonbuzz <fonbuzz0@gmail.com>
2022-10-15 22:44:30 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the last digit of the number stored
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n <= 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}

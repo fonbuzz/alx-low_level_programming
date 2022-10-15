cf0edb9821518cda3217c613db89b7bbb80a4d3f
fonbuzz <fonbuzz0@gmail.com>
2022-10-15 23:32:39 +0100

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of base 16
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int base16;
+
+	for (base16 = 0; base16 <= 9; base16++)
+	{
+		putchar((base16 % 10) + '0);
+		if (base == 9)
+			continue;
+		putchar(', ');
+		putchar(' ');
+	}
+
+	putchar('\n);
+	return (0);
+}

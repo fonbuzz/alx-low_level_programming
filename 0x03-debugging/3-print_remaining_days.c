6c869642951cd14a29b99a1e30a31c8639ef40a6
fonbuzz <fonbuzz0@gmail.com>
2022-10-20 22:34:53 +0100

    largest

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are left 
+* print_remaining_days - takes a date and prints how many days are left
 * in the year, taking leap years into consideration
 * @month: month in number format
 * @day: day of month

ad38b32e51ed022c48db3e000fd90074da2ba3d6
Your Name <you@example.com>
2022-10-20 11:18:27 -0700

    supp0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _isdigit - write a function that check for a digit (0 through 9).
+* _isdigit - writes a function that check for a digit (0 through 9).
 * @c: char to check the digit
 *
 * Return:  0 or 1
-*/
+**/
 
 int _isdigit(int c)
 {

9e622386ddf8949f04c570cde278bc954d23921a
fonbuzz <fonbuzz0@gmail.com>
2022-10-24 22:54:58 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
  * _strcpy - copy the string pointed to by src to dest
  * @dest: char to check
  * @src: char to check
- * Return: 0 is success
+ * Return: 0
  */
 char *_strcpy(char *dest, char *src)
 {

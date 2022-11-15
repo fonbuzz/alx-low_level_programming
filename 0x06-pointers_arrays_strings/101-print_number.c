f3db7463da2a60a015a609c1bb2b639247cf686d
fonbuzz <fonbuzz0@gmail.com>
2022-11-15 12:06:57 +0100

    printnumz

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,10 +8,10 @@
  */
 void print_number(int n)
 {
-	for (i = 100; i > 1; i/10)
+	for (i = 100; i > 1; i / 10)
 {
-		putchar(n/i + '0');
-		n = n%i;
-		if (i==10)
+		putchar(n / i + '0');
+		n = n % i;
+		if (i == 10)
 			putchar(n + '0')
 }

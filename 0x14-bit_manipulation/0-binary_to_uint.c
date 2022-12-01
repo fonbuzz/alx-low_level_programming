a87920a44f92963e08a832b2b7e692ef82a80842
fonbuzz <35838372+fonbuzz@users.noreply.github.com>
2022-12-01 22:15:50 +0100

    unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int digit = 0;
-if (!b)
-return (0);
-for (i = 0; b[i]; i++)
-{
-if (b[i] != '0' && b[i] != '1')
-return (0);
-digit = 2 * digit + (b[i] - '0');
-}
-return (digit);
+	int i;
+	unsigned int j;
+
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }

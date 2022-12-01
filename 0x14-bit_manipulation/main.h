becec19e56a36df4525df00b059e726086e5f7f2
fonbuzz <35838372+fonbuzz@users.noreply.github.com>
2022-12-01 22:35:48 +0100

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
-unsigned long int _power(unsigned int base, unsigned int pow);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
